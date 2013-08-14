ac23350ef13a4ba1e5c612d2f0caae81100b8f2e
Xiao Nan <road2stat@gmail.com>
2013-08-14 17:56:42 +0800

    getcpi and getppi done

--- R/601-getCPI.R
+++ R/601-getCPI.R
@@ -1,5 +1,76 @@
-getCPI = function () {
+inner = function (a, b, f, ...) {
+  f = match.fun(f)
+  apply(b, 2, function(x) apply(a, 1, function(y) f(x, y, ...)))
+}
+
+getCPICombine = function (drugmat, protmat) {
   
-  return(NULL)
+  return(cbind(drugmat, protmat))
   
 }
+
+getCPITensor = function (drugmat, protmat, row, dcol, pcol) {
+  
+  return(array(inner(t(drugmat), protmat, '*'), c(row, dcol * pcol)))
+
+}
+
+getCPI = function (drugmat, protmat, type = c('combine', 'tensorprod')) {
+  
+  if (!is.matrix(drugmat)) drugmat = as.matrix(drugmat)
+  if (!is.matrix(protmat)) protmat = as.matrix(protmat)
+  
+  drugrow = nrow(drugmat)
+  protrow = nrow(protmat)
+  
+  if (drugrow != protrow) stop('Matrix row count must match')
+  
+  drugcol = ncol(drugmat)
+  protcol = ncol(protmat)
+  
+  if (missing(type)) stop('Must provide at least one interaction type')
+  
+  if (all(type == 'combine')) {
+    
+    result = getCPICombine(drugmat, protmat)
+    
+  } else if (all(type == 'tensorprod')) {
+    
+    result = getCPITensor(drugmat, protmat, row = drugrow, dcol = drugcol, pcol = protcol)
+    
+  } else if (length(setdiff(type, c('tensorprod', 'combine'))) == 0L) {
+    
+    result = cbind(getCPICombine(drugmat, protmat), getCPITensor(drugmat, protmat, row = drugrow, dcol = drugcol, pcol = protcol))
+    
+  } else {
+    
+    stop('Interaction type must be in "tensorprod" and "combine" or both')
+    
+  }
+  
+  return(result)
+  
+}
+
+
+# x = matrix(1:10, ncol = 2)
+# y = matrix(1:15, ncol = 3)
+# 
+# getCPI(x, y)
+# getCPI(x, y, 'combine')
+# getCPI(x, y, 'tensorprod')
+# getCPI(x, y, type = c('combine', 'tensorprod'))
+# getCPI(x, y, type = c('tensorprod', 'combine'))
+# getCPI(x, y, type = c('cbm'))
+
+## column-by-column (pseudo)-tensor product interactions
+## 
+# x = matrix(1:10, ncol = 2)
+# y = matrix(1:15, ncol = 3)
+# 
+# inner = function (a, b, f, ...) {
+#   f = match.fun(f)
+#   apply(b, 2, function(x) apply(a, 1, function(y) f(x, y, ...)))
+# }
+# 
+# array(inner(t(x), y, '*'), c(5, 6))
--- R/602-getPPI.R
+++ R/602-getPPI.R
@@ -1,5 +1,85 @@
-getPPI = function () {
+getPPICombine = function (protmat1, protmat2) {
   
-  return(NULL)
+  return(cbind(protmat1, protmat2))
   
 }
+
+getPPITensor = function (protmat1, protmat2, row, col) {
+  
+  return(array(inner(t(protmat1), protmat2, '*'), c(row, col^2)))
+  
+}
+
+getPPIEntry = function (protmat1, protmat2) {
+  
+  return(cbind((protmat1 * protmat2), (protmat1 + protmat2)))
+  
+}
+
+getPPI = function (protmat1, protmat2, type = c('combine', 'tensorprod', 'entrywise')) {
+  
+  if (!is.matrix(protmat1)) protmat1 = as.matrix(protmat1)
+  if (!is.matrix(protmat2)) protmat2 = as.matrix(protmat2)
+  
+  protrow1 = nrow(protmat1)
+  protrow2 = nrow(protmat2)
+  
+  if (protrow1 != protrow2) stop('Matrix row count must match')
+  
+  protcol1 = ncol(protmat1)
+  protcol2 = ncol(protmat2)
+  
+  if (protcol1 != protcol2) stop('Matrix column count must match')
+  
+  if (missing(type)) stop('Must provide at least one interaction type')
+  
+  if (all(type == 'combine')) {
+    
+    result = getPPICombine(protmat1, protmat2)
+    
+  } else if (all(type == 'tensorprod')) {
+    
+    result = getPPITensor(protmat1, protmat2, row = protrow1, col = protcol1)
+    
+  } else if (all(type == 'entrywise')) {
+    
+    result = getPPIEntry(protmat1, protmat2)
+    
+  } else if (length(setdiff(type, c('tensorprod', 'combine'))) == 0L) {
+    
+    result = cbind(getPPICombine(protmat1, protmat2), getPPITensor(protmat1, protmat2, row = protrow1, col = protcol1))
+    
+  } else if (length(setdiff(type, c('tensorprod', 'entrywise'))) == 0L) {
+    
+    result = cbind(getPPITensor(protmat1, protmat2, row = protrow1, col = protcol1), getPPIEntry(protmat1, protmat2))
+    
+  } else if (length(setdiff(type, c('combine', 'entrywise'))) == 0L) {
+    
+    result = cbind(getPPICombine(protmat1, protmat2), getPPIEntry(protmat1, protmat2))
+    
+  } else if (length(setdiff(type, c('tensorprod', 'combine', 'entrywise'))) == 0L) {
+    
+    result = cbind(getPPICombine(protmat1, protmat2), getPPITensor(protmat1, protmat2, row = protrow1, col = protcol1), getPPIEntry(protmat1, protmat2))
+    
+  } else {
+    
+    stop('Interaction type must be in "tensorprod", "combine" and "entrywise"')
+    
+  }
+  
+  return(result)
+  
+}
+
+# x = matrix(1:10, ncol = 2)
+# y = matrix(5:14, ncol = 2)
+# 
+# getPPI(x, y)
+# getPPI(x, y, type = 'combine')
+# getPPI(x, y, type = 'tensorprod')
+# getPPI(x, y, type = 'entrywise')
+# getPPI(x, y, type = c('combine', 'tensorprod'))
+# getPPI(x, y, type = c('combine', 'entrywise'))
+# getPPI(x, y, type = c('entrywise', 'tensorprod'))
+# getPPI(x, y, type = c('combine', 'entrywise', 'tensorprod'))
+# getPPI(x, y, type = c('combine', 'entrywise', 'ccc'))
