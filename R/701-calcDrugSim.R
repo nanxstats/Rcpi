fcfd70f30a5b9f796d57d369d13ff0d7de81e850
Xiao Nan <road2stat@gmail.com>
2013-08-30 16:43:47 +0800

    finished main functions

--- R/701-calcDrugSim.R
+++ R/701-calcDrugSim.R
@@ -1,312 +1,103 @@
-calcDrugSim = function (fp1, fp2, type, 
-                        fptype = c('compressed', 'complete'), ...) {
+# Reference:
+# Gasteiger, Johann, and Thomas Engel, eds. Chemoinformatics. Wiley.com, 2006.
+
+# Let a be the features of object A, b is the features of object B, 
+# c is the number of common features to A and B
+
+# stanimoto: aka Jaccard c/a+b+c
+# seuclidean: no aka \sqrt(a + b)
+# dice: aka Sorensen, Czekanowski, Hodgkin-Richards c/0.5[(a+c) + (b+c)]
+# cosine: aka Ochiai, Carbo c/\sqrt((a + c)(b + c))
+# hamming: aka Manhattan, taxi-cab, city-block distance (a + b)
+
+# # Examples
+# require(rcdk)
+# smiles = c('C1CCC1CC(CN(C)(C))CC(=O)CC', 'c1ccccc1Cc1ccccc1')
+# mols = parse.smiles(smiles)
+# 
+# fp1 = extractDrugEstate(mols[[1]])
+# fp2 = extractDrugEstate(mols[[2]])
+# calcDrugSim(fp1, fp2, fptype = 'compressed', metric = 'tanimoto')
+# calcDrugSim(fp1, fp2, fptype = 'compressed', metric = 'euclidean')
+# calcDrugSim(fp1, fp2, fptype = 'compressed', metric = 'cosine')
+# calcDrugSim(fp1, fp2, fptype = 'compressed', metric = 'dice')
+# calcDrugSim(fp1, fp2, fptype = 'compressed', metric = 'hamming')
+# 
+# fp3 = extractDrugEstateComplete(mols[[1]])
+# fp4 = extractDrugEstateComplete(mols[[2]])
+# calcDrugSim(fp3, fp4, fptype = 'complete', metric = 'tanimoto')
+# calcDrugSim(fp3, fp4, fptype = 'complete', metric = 'euclidean')
+# calcDrugSim(fp3, fp4, fptype = 'complete', metric = 'cosine')
+# calcDrugSim(fp3, fp4, fptype = 'complete', metric = 'dice')
+# calcDrugSim(fp3, fp4, fptype = 'complete', metric = 'hamming')
+
+
+
+calcDrugSim = function (fp1, fp2, fptype = c('compressed', 'complete'), 
+                        metric = c('tanimoto', 'euclidean', 'cosine', 'dice', 'hamming')) {
   
-  return(NULL)
+  fptype = match.arg(fptype)
+  metric = match.arg(metric)
   
-}
-
-
-
-calcDrugSimMat = function (fplist1, fplist2 = NULL, type, 
-                           fptype = c('compressed', 'complete'), 
-                           fill = c('upper', 'lower', 'all'),
-                           ...) {
+  if ( !(fptype %in% c('compressed', 'complete')) ) stop('fptype must be one of "compressed" or "complete"')
+  if ( !(metric %in% c('tanimoto', 'euclidean', 'cosine', 'dice', 'hamming')) ) stop('metric must be one of "tanimoto", "euclidean", "cosine", "dice", "hamming"')
   
-  return(NULL)
-  
-}
-
-
-
-
-
-function (fp1, fp2, method, a, b) standardGeneric('distance')
-
-
-
-
-
-# 条件
-# c("featvec", "featvec", "missing", "missing", "missing")
-
-function (fp1, fp2) distance(fp1, fp2, 'tanimoto')
-
-
-
-
-
-# c("featvec", "featvec", "character", "missing", "missing")
-          
-function (fp1, fp2, method = c('tanimoto', 'dice', 'robust')) {
-  
-  method = match.arg(method)
-  n1 = length(fp1)
-  n2 = length(fp2)
-  n12 = length(intersect(fp1@features, fp2@features))
-  
-  if (method == 'tanimoto') {
+  if (fptype == 'compressed') {
     
-    return(n12/(n1 + n2 - n12))
+    fp1ext = integer(as.integer(names(fp1)))
+    fp1ext[fp1[[1]]] = 1L
+    fp2ext = integer(as.integer(names(fp2)))
+    fp2ext[fp2[[1]]] = 1L
     
-    } else if (method == 'robust') {
-      
-      return(0.5 + 0.5 * n12 * n12 / (n1 * n2))
-      
-      } else if (method == 'dice') {
-        
-        return(2.0 * n12 / (n1 + n2))
-      
-      }
-
-}
-
-
-
-
-
-# c("fingerprint", "fingerprint", "missing", "missing", "missing")
-
-function (fp1, fp2) {
-  
-  distance(fp1, fp2, 'tanimoto')
-
-}
-
-
-
-
-
-# c("fingerprint", "fingerprint", "character", "numeric", "numeric")
-
-function(fp1, fp2, type = 'tversky', a, b) {
-  
-  if (!is.null(method) && !is.na(method) && method != 'tversky') distance(fp1, fp2, method)
-  
-  if ( length(fp1) != length(fp2)) stop('Fingerprints must of the same bit length')
-  
-  if ( a < 0 || b < 0 ) stop('a and b must be positive')
-  
-  tmp = fp1 & fp2
-  xiy = length(tmp@bits)
-  tmp = fp1 | fp2
-  xuy = length(tmp@bits)
-  
-  x = length(fp1@bits)
-  y = length(fp2@bits)
-  
-  return( xiy / (a * x + b * y + (1 - a - b) * xiy ) )
-  
-}
-
-
-
-
-
-# c("fingerprint", "fingerprint", "character", "missing", "missing")
-
-function(fp1, fp2, method = c('tanimoto', 'euclidean', 'mt',
-                              'simple', 'jaccard', 'dice',
-                              'russelrao', 'rodgerstanimoto','cosine',
-                              'achiai', 'carbo', 'baroniurbanibuser',
-                              'kulczynski2', 'hamming', 'meanHamming', 
-                              'soergel', 'patternDifference', 'variance', 
-                              'size', 'shape', 'hamann', 'yule', 
-                              'pearson', 'dispersion', 'mcconnaughey', 
-                              'stiles', 'simpson', 'petke', 
-                              'stanimoto', 'seuclidean')) {
-  
-  if (method == 'tversky') stop('If Tversky metric is desired, must specify a and b')
-  
-  if ( length(fp1) != length(fp2) ) stop('Fingerprints must of the same bit length')
-  
-  method = match.arg(method)
-  n = length(fp1)
-  
-  if (method == 'tanimoto') {
+    fp1 = fp1ext
+    fp2 = fp2ext
     
-    f1 = numeric(n)
-    f2 = numeric(n)
-    f1[fp1@bits] = 1
-    f2[fp2@bits] = 1
-    sim = 0.0
+    if ( length(fp1) != length(fp2) ) stop('fingerprint vectors must have the same length')
     
-    ret = fpdist(f1, f2, 'tanimoto')
+    fp1 = as.logical(fp1)
+    fp2 = as.logical(fp2)
     
-    return (ret)
+    A = sum( (fp1 | fp2) & !fp2 )
+    B = sum( (fp1 | fp2) & !fp1 )
+    C = sum( fp1 & fp2 )
+    D = sum( !(fp1 | fp2) )
     
-    } else if (method == 'euclidean') {
-      
-      f1 = numeric(n)
-      f2 = numeric(n)
-      f1[fp1@bits] = 1
-      f2[fp2@bits] = 1
-      sim = 0.0
-      
-      ret = fpdist(fp1, fp2, 'euclidean')
-      
-      return (ret)
+    if (metric == 'tanimoto') dis = C/(A + B + C)
     
-    }
-  
-  size = n
-  
-  ## in A & B
-  tmp = fp1 & fp2
-  c = length(tmp@bits)
-  
-  ## in A not in B
-  tmp = (fp1 | fp2) & !fp2
-  a = length(tmp@bits)
-  
-  ## in B not in A
-  tmp = (fp1 | fp2) & !fp1
-  b = length(tmp@bits)
-  
-  ## not in A, not in B
-  tmp = !(fp1 | fp2)
-  d = length(tmp@bits)
-  dist = NULL
-  
-  ## Simlarity
-  if (method == 'stanimoto') {
+    if (metric == 'euclidean') dis = sqrt( (C + D) / (A + B + C + D) )
     
-    dist = c / (a + b + c)
+    if (metric == 'cosine') dis = C / sqrt( ( A + C ) * ( B + C ) )
     
-    } else if (method == 'seuclidean') {
-      
-      dist = sqrt((d + c) / (a + b + c + d))
-      
-      } else if (method == 'dice') {
-        
-        dist = c / (0.5 * a + 0.5 * b + c)
-        
-        } else if (method == 'mt') {
-          
-          t1 = c/(size - d)
-          t0 = d/(size - c)
-          phat = ((size - d) + c)/(2 * size)
-          dist = (2 - phat) * t1/3 + (1 + phat) * t0/3
-          
-          } else if (method == 'simple') {
-            
-            dist = (c + d)/n
-            
-            } else if (method == 'jaccard') {
-              
-              dist = c/(a + b + c)
-              
-              } else if (method == 'russelrao') {
-                
-                dist = c/size
-                
-                } else if (method == 'rodgerstanimoto') {
-                  
-                  dist = (c + d)/(2 * a + 2 * b + c + d)
-                  
-                  } else if (method == 'cosine' || method == 'achiai' || method == 'carbo') {
-                    
-                    dist = c/sqrt((a + c) * (b + c))
-                    
-                    } else if (method == 'baroniurbanibuser') {
-                      
-                      dist = (sqrt(c * d) + c)/(sqrt(c * d) + a + b + c)
-                      
-                      } else if (method == 'kulczynski2') {
-                        
-                        dist = 0.5 * (c/(a + c) + c/(b + c))
-                        
-                        } else if (method == 'hamming') { ## Dissimilarity
-                          
-                          dist = a + b
-                          
-                          } else if (method == 'meanHamming') {
-                            
-                            dist = (a + b)/(a + b + c + d)
-                            
-                            } else if (method == 'soergel') {
-                              
-                              dist = (a + b)/(a + b + c)
-                              
-                              } else if (method == 'patternDifference') {
-                                
-                                dist = (a * b)/(a + b + c + d)^2
-                                
-                                } else if (method == 'variance') {
-                                  
-                                  dist = (a + b)/(4 * n)
-                                  
-                                  } else if (method == 'size') {
-                                    
-                                    dist =  (a - b)^2/n^2
-                                    
-                                    } else if (method == 'shape') {
-                                      
-                                      dist = (a + b)/n - ((a - b)/(n))^2
-                                      
-                                      } else if (method == 'hamann') { ## Composite
-                                        
-                                        dist = (c + d - a - b)/(a + b + c + d)
-                                        
-                                        } else if (method == 'yule') {
-                                          
-                                          dist =  (c * d - a * b)/(c * d + a * b)
-                                          
-                                          } else if (method == 'pearson') {
-                                            
-                                            dist = (c * d - a * b)/sqrt((a + c) * (b + c) * (a + d) * (b + d))
-                                            
-                                            } else if (method == 'dispersion') {
-                                              
-                                              dist = (c * d - a * b)/n^2
-                                              
-                                              } else if (method == 'mcconaughey') {
-                                                
-                                                dist = (c^2 - a * b)/((a + c) * (b + c))
-                                                
-                                                } else if (method == 'stiles') {
-                                                  
-                                                  dist = log10(n * (abs(c * d - a * b) - n/2)^2/((a + c) * (b + c) * (a + d) * (b + d)))
-                                                  
-                                                  } else if (method == 'simpson') { ## Asymmetric
-                                                    
-                                                    dist = c/min((a + c), (b + c))
-                                                    
-                                                    } else if (method == 'petke') {
-                                                      
-                                                      dist = c/max((a + c), (b + c))
-                                                    
-                                                    }
-  
-  return(dist)
-
-}
-
-
-
-
-
-fpdist = function (fp1, fp2, metric = c('tanimoto', 'euclidean')) {
-  
-  n = length(fp1)
-  
-  if (metric == 'tanimoto') {
+    if (metric == 'dice') dis = C / ( 0.5 * ( (A + C) + (B + C) ) )
     
-    C = sum(fp1 == 1L & fp2 == 1L)
-    distance = C/(sum(fp1 != fp2) + C)
+    if (metric == 'hamming') dis = (A + B) / (A + B + C + D)
     
   }
   
-  if (metric == 'euclidean') {
+  if (fptype == 'complete') {
     
-    distance = sqrt(sum(fp1 == fp2)/n)
-  
+    if ( length(fp1) != length(fp2) ) stop('fingerprint vectors must have the same length')
+    
+    fp1 = as.logical(fp1)
+    fp2 = as.logical(fp2)
+    
+    A = sum( (fp1 | fp2) & !fp2 )
+    B = sum( (fp1 | fp2) & !fp1 )
+    C = sum( fp1 & fp2 )
+    D = sum( !(fp1 | fp2) )
+    
+    if (metric == 'tanimoto') dis = C/(A + B + C)
+    
+    if (metric == 'euclidean') dis = sqrt( (C + D) / (A + B + C + D) )
+    
+    if (metric == 'cosine') dis = C / sqrt( ( A + C ) * ( B + C ) )
+    
+    if (metric == 'dice') dis = C / ( 0.5 * ( (A + C) + (B + C) ) )
+    
+    if (metric == 'hamming') dis = (A + B) / (A + B + C + D)
+    
   }
   
-  return(distance)
+  return(dis)
   
 }
-
-# fp1 = c(1, 0, 1, 0, 0, 0)
-# fp2 = c(1, 0, 0, 0, 1, 1)
-# fpdist(fp1, fp2, 'tanimoto')
-# fpdist(fp1, fp2, 'euclidean')
