9bd452e81215c01963d985793b4ee86841add9a3
Nan Xiao <road2stat@gmail.com>
2017-06-21 11:22:00 -0400

    add xlogp unit test

---
+++ inst/unitTests/test_extractDrugXLogP.R
@@ -0,0 +1,12 @@
+test_extractDrugXLogP = function() {
+    smi = c(
+        'c1ccc2c(c1)oc(=O)c(C(CC(=O)C)c1ccc([N+](=O)[O-])cc1)c2O',
+        'c1(ccc(cc1)O)NC(=O)C',
+        'c1ccccc1NC(=O)C',
+        'n1nc(S(=O)(=O)N)sc1NC(=O)C',
+        'O=S(=O)(c1ccc(cc1)C(=O)C)NC(=O)NC1CCCCC1',
+        'CC(=O)NO')
+    mol = readMolFromSmi(textConnection(smi), type = 'mol')
+    dat = extractDrugXLogP(mol)
+    checkEqualsNumeric(sum(as.matrix(dat)), 6.884, tolerance = 1e-3)
+}
