28452d055e7c0b457b2cbe97f3a463e02a1601ac
Nan Xiao <road2stat@gmail.com>
2019-02-24 21:24:56 -0500

    fix go similarity issues

--- R/703-calcProtGOSim.R
+++ R/703-calcProtGOSim.R
@@ -1,7 +1,27 @@
-.calcgoPairSim = function(
-    twoid, golist = golist,
-    ont = ont, organism = organism, measure = measure,
-    combine = combine) {
+.orgdb = c(
+    'anopheles' = 'org.Ag.eg.db',
+    'arabidopsis' = 'org.At.tair.db',
+    'bovine' = 'org.Bt.eg.db',
+    'canine' = 'org.Cf.eg.db',
+    'chicken' = 'org.Gg.eg.db',
+    'chimp' = 'org.Pt.eg.db',
+    'coelicolor' = 'org.Sco.eg.db',
+    'ecolik12' = 'org.EcK12.eg.db',
+    'ecsakai' = 'org.EcSakai.eg.db',
+    'fly' = 'org.Dm.eg.db',
+    'human' = 'org.Hs.eg.db',
+    'malaria' = 'org.Pf.plasmo.db',
+    'mouse' = 'org.Mm.eg.db',
+    'pig' = 'org.Ss.eg.db',
+    'rat' = 'org.Rn.eg.db',
+    'rhesus' = 'org.Mmu.eg.db',
+    'worm' = 'org.Ce.eg.db',
+    'xenopus' = 'org.Xl.eg.db',
+    'yeast' = 'org.Sc.sgd.db',
+    'zebrafish' = 'org.Dr.eg.db'
+)
+
+.calcgoPairSim = function(twoid, golist, godb, measure, combine) {
 
     id1 = twoid[1]
     id2 = twoid[2]
@@ -19,12 +39,9 @@
         gid2 = as.character(golist[[id2]][id2good])
 
         res = try(suppressWarnings(GOSemSim::mgoSim(
-            gid1, gid2,
-            ont = ont,
-            organism = organism,
-            measure = measure,
-            combine = combine)),
-            silent = TRUE)
+            gid1, gid2, semData = godb, measure = measure, combine = combine
+        )),
+        silent = TRUE)
 
         if ( is.numeric(res) ) {
             sim = res
@@ -34,7 +51,7 @@
 
     }
 
-    return(sim)
+    sim
 
 }
 
@@ -79,29 +96,30 @@
 #' @export calcParProtGOSim
 #'
 #' @examples
-#' # by GO Terms
+#' # By GO Terms
 #' go1 = c('GO:0005215', 'GO:0005488', 'GO:0005515', 'GO:0005625', 'GO:0005802', 'GO:0005905')  # AP4B1
 #' go2 = c('GO:0005515', 'GO:0005634', 'GO:0005681', 'GO:0008380', 'GO:0031202')  # BCAS2
 #' go3 = c('GO:0003735', 'GO:0005622', 'GO:0005840', 'GO:0006412')  # PDE4DIP
 #' glist = list(go1, go2, go3)
-#' \donttest{
-#' gsimmat1 = calcParProtGOSim(glist, type = 'go', ont = 'CC')
-#' print(gsimmat1)}
+#' \donttest{calcParProtGOSim(glist, type = 'go', ont = 'CC', measure = 'Wang')}
 #'
-#' # by Entrez gene id
+#' # By Entrez gene id
 #' genelist = list(c('150', '151', '152', '1814', '1815', '1816'))
-#' \donttest{
-#' gsimmat2 = calcParProtGOSim(genelist, type = 'gene')
-#' print(gsimmat2)}
+#' \donttest{calcParProtGOSim(genelist, type = 'gene', ont = 'BP', measure = 'Wang')}
 
 calcParProtGOSim = function(
     golist, type = c('go', 'gene'),
-    ont = 'MF', organism = 'human',
+    ont = c('MF', 'BP', 'CC'), organism = 'human',
     measure = 'Resnik', combine = 'BMA') {
+
+    type = match.arg(type)
+    ont = match.arg(ont)
+
+    godb = suppressMessages(GOSemSim::godata(OrgDb = .orgdb[organism], ont = ont, computeIC = TRUE))
 
     if ( type == 'gene' ) {
         gosimmat = GOSemSim::mgeneSim(
-            unlist(golist), ont = ont, organism = organism,
+            unlist(golist), godb,
             measure = measure, combine = combine, verbose = FALSE)
     }
 
@@ -115,7 +133,7 @@ calcParProtGOSim = function(
 
         for ( i in 1:ncol(idx) ) {
             gosimlist[[i]] = .calcgoPairSim(
-                rev(idx[, i]), golist = golist, ont = ont, organism = organism,
+                rev(idx[, i]), golist = golist, godb,
                 measure = measure, combine = combine)
         }
 
@@ -127,7 +145,7 @@ calcParProtGOSim = function(
 
     }
 
-    return(gosimmat)
+    gosimmat
 
 }
 
@@ -175,39 +193,41 @@ calcParProtGOSim = function(
 #' @export calcTwoProtGOSim
 #'
 #' @examples
-#' # by GO terms
+#' # By GO terms
 #' go1 = c("GO:0004022", "GO:0004024", "GO:0004023")
 #' go2 = c("GO:0009055", "GO:0020037")
-#' \donttest{
-#' gsim1 = calcTwoProtGOSim(go1, go2, type = 'go', ont = 'MF', measure = 'Wang')
-#' print(gsim1)}
+#' \donttest{calcTwoProtGOSim(go1, go2, type = 'go', ont = 'MF', measure = 'Wang')}
 #'
-#' # by Entrez gene id
+#' # By Entrez gene id
 #' gene1 = '241'
 #' gene2 = '251'
-#' \donttest{
-#' gsim2 = calcTwoProtGOSim(gene1, gene2, type = 'gene', ont = 'CC', measure = 'Lin')
-#' print(gsim2)}
+#' \donttest{calcTwoProtGOSim(gene1, gene2, type = 'gene', ont = 'CC', measure = 'Lin')}
 
 calcTwoProtGOSim = function(
     id1, id2, type = c('go', 'gene'),
-    ont = 'MF', organism = 'human',
+    ont = c('MF', 'BP', 'CC'), organism = 'human',
     measure = 'Resnik', combine = 'BMA') {
+
+    type = match.arg(type)
+    ont = match.arg(ont)
+
+    godb = suppressMessages(GOSemSim::godata(OrgDb = .orgdb[organism], ont = ont, computeIC = TRUE))
 
     if ( type == 'go' ) {
         sim = GOSemSim::mgoSim(
             id1, id2,
-            ont = ont, organism = organism,
+            semData = godb,
             measure = measure, combine = combine)
     }
 
     if ( type == 'gene' ) {
         sim = GOSemSim::geneSim(
             id1, id2,
-            ont = ont, organism = organism,
-            measure = measure, combine = combine)$geneSim
+            semData = godb,
+            measure = measure, combine = combine)
+        if ("geneSim" %in% names(sim)) sim = sim$geneSim
     }
 
-    return(sim)
+    sim
 
 }
